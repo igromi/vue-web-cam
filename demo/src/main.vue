93ae8e0ea241a6f0af7507db398b6eb2b4775204
agui1era <oscar@aguilera.xyz>
2021-03-15 19:24:51 -0300

    Fix

--- demo/src/main.vue
+++ demo/src/main.vue
@@ -16,16 +16,7 @@
                     <th>
 
                      <div class="container ">   
-                     <vue-web-cam
-                        ref="webcam"
-                        :device-id="deviceId"
-                        width="100%"
-                        @started="onStarted"
-                        @stopped="onStopped"
-                        @error="onError"
-                        @cameras="onCameras"
-                        @camera-change="onCameraChange"
-                    />
+                     <img  v-bind:src="urlIMG">
                  
                     </div>
                     </th>
@@ -53,9 +44,6 @@ import axios from 'axios';
 
 export default {
     name: "App",
-    components: {
-        "vue-web-cam": WebCam
-    },
     data() {
         return {
             img: null,
@@ -65,6 +53,7 @@ export default {
             color1Cam1: "",
             color2Cam1: "",
             urlAPI:"http://localhost/data.json",
+            urlIMG:"./uvas.jpg",
             info:""
         };
     },
@@ -79,58 +68,24 @@ export default {
             return this.devices.find(n => n.deviceId === this.deviceId);
         }
     },
-    watch: {
-        camera: function(id) {
-            this.deviceId = id;
-        },
-        devices: function() {
-            // Once we have a list select the first one
-            const [first,second, ...tail] = this.devices;
-            if (first) {
-                this.camera = second.deviceId;
-                this.deviceId = second.deviceId;
-            }
-        }
-    },
     methods: {
-        onCapture() {
-            this.img = this.$refs.webcam.capture();           
-        },
-        onStarted(stream) {
-            console.log("On Started Event", stream);
-        },
-        onStopped(stream) {
-            console.log("On Stopped Event", stream);
-        },
-        onStop() {
-            this.$refs.webcam.stop();
-        },
-        onStart() {
-            this.$refs.webcam.start();
-        },
-        onError(error) {
-            console.log("On Error Event", error);
-        },
-        onCameras(cameras) {
-            this.devices = cameras;
-            console.log("On Cameras Event", cameras);
-        },
-        onCameraChange(deviceId) {
-            this.deviceId = deviceId;
-            this.camera = deviceId;
-            console.log("On Camera Change Event", deviceId);
-        },
+
         loadData() {
             axios
             .get(this.urlAPI)
             .then(response => (this.info = response));
            
 
-           let negras = parseInt(this.info.data.CAM1.negra)
-           let rosadas = parseInt(this.info.data.CAM1.rosada)
+           let negras = parseInt(this.info.data.CAM1.negra);
+           let rosadas = parseInt(this.info.data.CAM1.rosada);
 
-           this.color1Cam1= Math.trunc(negras/(negras+rosadas)*100)
-           this.color2Cam1= Math.trunc(rosadas/(negras+rosadas)*100)
+           this.color1Cam1= Math.trunc(negras/(negras+rosadas)*100);
+           this.color2Cam1= Math.trunc(rosadas/(negras+rosadas)*100);
+           
+           this.urlIMG="./uvas.jpg";
+           this.urlIMG="";
+           this.urlIMG="./uvas.jpg";
+
         },
     }
 };
